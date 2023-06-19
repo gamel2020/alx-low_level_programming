d0db50e3b8d66492308eb361dcf7b8d130c68f87
gamel2020 <gamelnashaat2018@gmail.com>
2023-06-19 16:48:36 -0700

    G

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 /**
  * main - Entry point
  *
@@ -6,6 +7,7 @@
  * 
  * Return: always 0 (success)
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
