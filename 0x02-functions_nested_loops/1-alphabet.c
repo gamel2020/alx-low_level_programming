0e309f08fb5c9ebc63873351f7018f8426e6d05c
gamel2020 <gamelnashaat2018@gmail.com>
2023-06-21 15:42:53 -0700

    G

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,11 +4,12 @@
  * print_alphabet - utilizes on the _putchar function to print
  * 	the alphabet a-z
 */
+
 void print_alphabet(void)
 {
 	int ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
-		_putchar(char);
+		_putchar(ch);
 	_putchar('\n');
 }
