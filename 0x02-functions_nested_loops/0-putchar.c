6e80c4692a6540cc32724c51d85128ba54040d72
gamel2020 <gamelnashaat2018@gmail.com>
2023-06-21 14:00:19 -0700

    G

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
 #include "main.h"
-
+#include <unistd.h>
 
 /**
- * main - entry point
- * Return: Always 0
-*/
-
+ * main - prints "_putchar" followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("_putchar");
-	return (0);
+    char str[] = "_putchar\n";
+    int len = sizeof(str) / sizeof(str[0]);
+    int i;
+
+    for (i = 0; i < len; i++)
+    {
+        write(STDOUT_FILENO, &str[i], 1);
+    }
+
+    return (0);
 }
