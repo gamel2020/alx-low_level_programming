ce5494aac8651d3ba011d6cc49f7505fc3b5bdc9
gamel2020 <gamelnashaat2018@gmail.com>
2023-06-21 16:02:48 -0700

    G

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,11 @@
 #include"main.h"
 #include <unistd.h>
-int _putchar(char c);
 #include <unistd.h>
+int _putchar(char c);
+
 /**
  * print_alphabet - utilizes on the _putchar function to print
- * 	the alphabet a-z
+ *	the alphabet a-z
 */
 
 void print_alphabet(void)
