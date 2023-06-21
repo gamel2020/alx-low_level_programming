d1bbe0e35bd480499d6a86ed5cf523b6c12965dd
gamel2020 <gamelnashaat2018@gmail.com>
2023-06-21 15:33:55 -0700

    G

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,13 @@
+#include"main.h"
+
+/**
+ * print_alphabet - utilizes on the _putchar function to print
+ * 	the alphabet a-z
+*/
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(char);
+	_putchar('\n');
+}
