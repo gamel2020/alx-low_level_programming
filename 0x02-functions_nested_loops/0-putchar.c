f9570047b885e7f0f0b804a2fb2149331687cad9
gamel2020 <gamelnashaat2018@gmail.com>
2023-06-21 13:33:26 -0700

    G

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,22 +8,6 @@
 
 int main(void)
 {
-	_putchar('-');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-<<<<<<< HEAD
-	_putchar('r');
-	_putchar(\n);
-	return (write(1, &c, 1));
+	printf("_putchar");
+	return (0);
 }
-
-=======
-	 _putchar('r');
-	_putchar(\n);
-	return (write(1, &c, 1));
-}
->>>>>>> 8721522e4aa5bf36626d1f241bbda5acc1a5281b
