a4db900f6670c917ceafb1a8f0f9b9ab9058c670
gamel2020 <gamelnashaat2018@gmail.com>
2023-06-26 16:29:31 -0700

    G

--- 0x05-pointers_arrays_strings/main.c
+++ 0x05-pointers_arrays_strings/main.c
@@ -8,11 +8,11 @@ void reset_to_98(int *);
 */
 int main(void)
 {
-	int n;
+        int n;
 
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+        n = 402;
+        printf("n=%d\n", n);
+        reset_to_98(&n);
+        printf("n=%d\n", n);
+        return (0);
 }
