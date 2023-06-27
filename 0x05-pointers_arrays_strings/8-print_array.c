754220f4cdf6bf576dad13a93adfc37066ad6410
gamel2020 <gamelnashaat2018@gmail.com>
2023-06-27 16:30:21 -0700

    G

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,24 +1,27 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - Entry point
+ * print_array - prints n elements of an arrayof integer
+ *
+ * @n: elements parameter input
+ * @a: string parameter input
  *
  * Return: Always 0 (Success)
- */
-int main(void)
+*/
+void print_array(int *a, int n)
 {
-    int array[5] = { -198, 298, 402, -1024, 98 };
-    int i;
+	int array[5] = { -198, 298, 402, -1024, 98 };
+	int i;
 
-    for (i = 0; i < sizeof(array) / sizeof(int); i++)
-    {
-        printf("%d", array[i]);
-        if (i != sizeof(array) / sizeof(int) - 1) {
-            printf(", ");
-        }
-    }
-    printf("\n");
+	for (i = 0; i < sizeof(array) / sizeof(int); i++)
+	{
+	printf("%d", array[i]);
+	if (i != sizeof(array) / sizeof(int) - 1)
+       	{
+	printf(", ");
+	}
+	}
+		printf("\n");
 
-    return (0);
+	return (0);
 }
