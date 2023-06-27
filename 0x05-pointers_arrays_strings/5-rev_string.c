44843b15da1082922fb6d8d57845a6648ab75a91
gamel2020 <gamelnashaat2018@gmail.com>
2023-06-27 15:31:12 -0700

    G

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,10 +16,10 @@ void rev_string(char *s)
 	for (l = 0; s[l] != '\0'; ++l)
 
 	/*swap the string by looping to half the string*/
-	for (i = 0; i<l / 2; i++)
+	for (i = 0; i < l / 2; i++)
 	{
 		temp = s[i];
-		s[i] = s[l-1-i]; /*-1 becouse the array starts from 0*/
+		s[i] = s[l - 1 -i]; /*-1 becouse the array starts from 0*/
 		s[l - 1 - i] = temp;
 	}       
 }
