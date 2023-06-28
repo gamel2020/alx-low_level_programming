85a3f129e4401669ccd978e988ed47926e2643ba
gamel2020 <gamelnashaat2018@gmail.com>
2023-06-28 09:06:57 -0700

    G

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all alphabet in lowercase than uppercase
+ *
+ * Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	char ch = 'a';
+	char ch = 'A';
+
+	/*print a - z*/
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+	}
+	/*print A - Z*/
+	while (CH <= 'Z')
+	{
+		putchar(ch);
+		CH++;
+	}
+	putchar('\n');
+	return (0);
+}
