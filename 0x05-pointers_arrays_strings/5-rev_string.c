6411f7b8fdac16d110ec6ab9608647906bec436a
gamel2020 <gamelnashaat2018@gmail.com>
2023-06-27 18:02:20 -0700

    G

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -15,8 +15,7 @@ void rev_string(char *s)
 
 	/*find string length without null char*/
 
-for (l = 0; s[l] != '\0'; ++l)
-
+	for (l = 0; s[i] != '\0'; ++l)
 			;
 
 	/*swap the string by looping to half the string*/
