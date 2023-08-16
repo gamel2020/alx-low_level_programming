82137f741c2aeaabc8dc929d0f530f4e0a1da7c5
gamel2020 <gamelnashaat2018@gmail.com>
2023-08-16 12:37:22 -0700

    G

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,66 @@
+#include "3_clac.h"
+
+/**
+ * 0p_add - adds tow integers
+ * @a: the first int
+ * @b: he second int
+ *
+ * Return: the sum
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtracts tow integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the differance
+*/
+
+int op_sub(int a, int b)
+{
+	return (a -b);
+}
+
+/**
+ * op_mul - multiplies tow integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the preduct
+*/
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides tow integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the quotient
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo tow integers
+ * @a: the first int
+ * @b: the second int
+ *
+ * Return: the remainder
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
