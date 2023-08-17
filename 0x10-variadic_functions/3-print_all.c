02cdb3ea9189f79c85d119840bde2dfcdb01ee11
gamel2020 <gamelnashaat2018@gmail.com>
2023-08-17 10:54:41 -0700

    G

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -57,7 +57,7 @@ void format_string(char *separator, va_list ap)
 
 void print_all(const char * const format, ...)
 {
-	| int i = 0, j;
+	int i = 0, j;
 	char *separator = "";
 	va_list ap;
 	token_t tokens[] = {
@@ -82,7 +82,6 @@ void print_all(const char * const format, ...)
 			j++;
 		}
 		i++;
-	|
 	}
 	printf("\n");
 	va_end(ap);
