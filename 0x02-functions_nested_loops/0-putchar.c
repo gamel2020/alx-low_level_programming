b1f25b7a0b27cebfcff5fe90a520cb20ecbdfd89
gamel2020 <gamelnashaat2018@gmail.com>
2023-09-29 08:19:22 -0700

    c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "main.h"
+
 /**
  * main - entry point
  *
- * Dscription: prints _putchar using putchar prototype
+ * description: prints_putchar prototype
  *
  * Return: Always 0 (success)
-*/
+ */
 
 int main(void)
 {
 	char str[] = "_putchar";
 	int ch;
 
-	for (ch = 0; ch < 8; ch++)
+	for (ch = 0, ch < 8; ch++)
 		_putchar(str[ch]);
 	_putchar('\n');
 
