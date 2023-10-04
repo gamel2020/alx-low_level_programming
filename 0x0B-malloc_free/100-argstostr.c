48a22a903dcef7b742ced2bd1a27a7f497726d03
gamel2020 <gamelnashaat2018@gmail.com>
2023-10-04 16:34:49 -0700

    G

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -8,11 +8,11 @@
  */
 int _strlen(char *s)
 {
-        int size = 0;
+	int size = 0;
 
-        for (; s[size] != '\0'; size++)
-        ;
-        return (size);
+	for (; s[size] != '\0'; size++)
+	;
+	return (size);
 }
 
 /**
@@ -23,26 +23,26 @@ int _strlen(char *s)
  */
 char *argstostr(int ac, char **av)
 {
-        int i, j, nc = 0, cmpt = 0;
-        char *s;
+	int i, j, nc = 0, cmpt = 0;
+	char *s;
 
-        if (ac == 0 || av == NULL)
-                return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-        for (i = 0; i < ac; i++)
-                nc += _strlen(av[i]);
+	for (i = 0; i < ac; i++)
+		nc += _strlen(av[i]);
 
-        s = malloc(sizeof(char) * (nc + ac + 1));
-        if (s == NULL)
-                return (NULL);
+	s = malloc(sizeof(char) * (nc + ac + 1));
+	if (s == NULL)
+		return (NULL);
 
-        for (i = 0; i < ac; i++)
-        {
-                for (j = 0; av[i][j] != '\0'; j++, cmpt++)
-                        s[cmpt] = av[i][j];
-                s[cmpt] = '\n';
-                cmpt++;
-        }
-        s[cmpt] = '\0';
-        return (s);
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++, cmpt++)
+			s[cmpt] = av[i][j];
+			s[cmpt] = '\n';
+		cmpt++;
+	}
+	s[cmpt] = '\0';
+	return (s);
 }
