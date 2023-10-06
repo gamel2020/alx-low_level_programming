bb96c172e91a8fe3a6f6ab4687603bf7e4234376
gamel2020 <gamelnashaat2018@gmail.com>
2023-10-05 23:56:42 -0700

    G

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,8 +2,8 @@
 
 
 /**
- * _abs - function that computes the absolute
- *	vlaue of an integer
+ * _abs - function that computes the absolute vlaue of an integer
+ *
  *
  * @n: takes in integer type input for function
  *
@@ -14,5 +14,5 @@ int _abs(int n)
 {
 	if (n < 0)
 		n = (-1) * n;
-	return (0);
+	return (n);
 }
