fd277b235eefc1116b9ee4665dd745fe03a70053
gamel2020 <gamelnashaat2018@gmail.com>
2023-10-07 04:17:02 -0700

    G

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,7 +3,8 @@
 /**
  * print_most_numbers - print 0 - 9 apart
  *	from 2 and 4 and you can
- *	only use_putchar
+ *	only use _putchar twice
+ *
  * Return: always 0 (success)
  */
 
@@ -13,9 +14,9 @@ void print_most_numbers(void)
 
 	for (num = 0; num <= 9; num++)
 	{
-		if (num == 2 || num = 4)
-			continue;
-		_putchar(num + 48);
+	if (num == 2 || num == 4)
+		continue;
+	_putchar(num + '0');
 	}
 	_putchar('\n');
 }
