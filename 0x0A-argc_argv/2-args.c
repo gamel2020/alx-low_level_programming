7c3517b0892e51433c7cdd46a92dd7c84e91468c
gamel2020 <gamelnashaat2018@gmail.com>
2023-10-08 14:18:40 -0700

    G

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,7 +2,7 @@
 
 
 /**
- * main - prints the number of arguments passed into the program 
+ * main - prints the number of arguments passed into the program
  * @argc: int
  * @argv: list
  * Return: 0
