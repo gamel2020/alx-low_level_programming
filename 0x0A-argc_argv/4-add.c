a2c19a2287843df26ac55942a39807b5324777a9
gamel2020 <gamelnashaat2018@gmail.com>
2023-10-08 14:30:25 -0700

    G

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -14,11 +14,11 @@ int main(int argc, char *argv[])
 	int sum = 0;
 	char *c;
 
-	while ( --argc)
+	while (--argc)
 	{
 		for (c = argv[argc]; *c; c++)
 			if (*c < '0' || *c > '9')
-				return (printf("Error\n", 1);
+				return (printf("Error\n"), 1);
 		sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
