970483b5147bdfafc9f77b474bf14b2848f22fca
gamel2020 <gamelnashaat2018@gmail.com>
2023-10-10 16:09:32 -0700

    G

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,10 +4,10 @@
  * init_dog - initializes a dog
  * @d: the dog to init
  * @name: the dog's name
- * @age: the dog's age
- * @owner: the dog's owner
+ * @age: dog's age
+ * @owner: dog's owner
  *
- * Return: void
+ * Return: void.
 */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
