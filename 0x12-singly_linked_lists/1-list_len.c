21eb507b9a0d6c1ee0b1c33a7405058fc0b113ce
gamel2020 <gamelnashaat2018@gmail.com>
2023-10-20 16:20:02 -0700

    G

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- * list_len - determines length of linked list
- * @h: pointer to first node
+ * list_len - determines the length of a linked list
+ * @h: pointer to the first node
  *
- * return: size of list
+ * Return: the number of nodes in the linked list
  */
 
 size_t list_len(const list_t *h)
