51bdbf6ac9711f70b9f71eb45df6cb9bb3dbf988
gamel2020 <gamelnashaat2018@gmail.com>
2023-10-20 17:49:20 -0700

    G

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,21 @@
-section		.text
-	extern	printf
-	global	main
+section .data
+	hello db "Hello, Holberton", 0
+	format db "%s", 10, 0
+
+section .text
+	global main
+	extern printf
 
 main:
-	mov		edi , msg
-	mov		eax , 0
-	call	printf
+	; Push the format string address onto the stack
+	push format
+	; Push the hello string address onto the stack
+	push hello
+	; Call printf
+	call printf
+	; Clean up the stack
+	add rsp, 16
 
-
-section		.data
-	msg alb 'Hello, Holberton' , 0xa, 0
+	; Exit the program
+	mov eax, 0
+	ret
