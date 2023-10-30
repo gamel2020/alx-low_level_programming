d2734ca56737b3f1156dda0dc8219031534e572f
gamel2020 <gamelnashaat2018@gmail.com>
2023-10-30 12:32:33 -0700

    G

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+	i++;
+	return (i);
+}
+
+/**
+ * append_text_to_file - appends text to a file
+ *
+ * @filename: The name of the file to create.
+ * @text_content: text to write
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes = 0, len = _strlen(text_content);
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content)
+	{
+		bytes = write(fd, text_content, len);
+		if (bytes == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	}
+
+	close(fd);
+	return (1);
+}
